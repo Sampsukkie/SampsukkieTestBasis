6945f22b04963ca7c5514d9cc58e5b6757f73215
Noel <117081212+Sampsukkie@users.noreply.github.com>
2023-08-31 10:12:37 +0300

    10.12

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    /* Muuttujat on j‰rjestetty seuraavasti:
+    /* Muuttujat on jarjestetty seuraavasti:
      * 
      * 1. static
      * 2. Numerot
      * 3. Unity
      * 4. Booleanit (true/false)
+     * 5. Muu
      * 
-     * Public muuttujat ovat ylh‰‰ll‰ (PascalCase) privaatit alhaalla (camelCase)
+     * Public muuttujat ovat ylhaalla (PascalCase) privaatit alhaalla (camelCase)
      * 
-     * !! Erona audioclipit
      * 
-     * Unity funktiot (Awake, Sart, jne.) n‰kyv‰t ennen muunneltuja 
+     * 
+     * Unity funktiot (Awake, Sart, jne.) nakyvat ennen muunneltuja 
      * (SetStartBoosting, SetStartingSound, jne.) funktioita
+     * 
+     * 
      */
 
+    public static PlayerController Instance;
+
+    private float playerSpeed;
+    private float boostRecovery = 4.5f;
+    private float nextBoost;
+
+    private AudioSource playerSource;
+
+    private Animator playerNator;
+
+    Rigidbody2D rb;
+
+    public void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
+    {
+        playerSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();
+        playerNator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.right * playerSpeed * Time.deltaTime;
+
+        if(rb.velocity.magnitude < playerSpeed)
+        {
+
+        }
+
+        if(Time.time > nextBoost)
+        {
+            Debug.Log("BOOST");
+            nextBoost = Time.time + boostRecovery;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         
     }
 
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collider)
     {
         
     }
