b0689a4694ffa7117c5f11bbec12fe67dbf2dc64
Noel <117081212+Sampsukkie@users.noreply.github.com>
2023-08-31 09:21:00 +0300

    9:20 Thu

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /* Muuttujat on j‰rjestetty seuraavasti:
+     * 
+     * 1. static
+     * 2. Numerot
+     * 3. Unity
+     * 4. Booleanit (true/false)
+     * 
+     * Public muuttujat ovat ylh‰‰ll‰ (PascalCase) privaatit alhaalla (camelCase)
+     * 
+     * !! Erona audioclipit
+     * 
+     * Unity funktiot (Awake, Sart, jne.) n‰kyv‰t ennen muunneltuja 
+     * (SetStartBoosting, SetStartingSound, jne.) funktioita
+     */
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
